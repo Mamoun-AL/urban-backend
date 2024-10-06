11ab9f19ce02f3f015411f7f797abb2417ccd21a
mamoun alrefai <77227904+Mamoun-AL@users.noreply.github.com>
2024-10-06 16:57:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: '*',
+  origin: 'https://urban-frontend.onrender.com',
   credentials: true
 }));
 
