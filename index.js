865f55ec9e385c04b72975c5461a1deca9b65829
mamoun alrefai <77227904+Mamoun-AL@users.noreply.github.com>
2024-10-07 12:18:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 // Middleware
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+
 
 app.use(express.json());
 app.use(cookieParser());
